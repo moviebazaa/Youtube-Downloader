e6357531e07bbd3e0595da42b12f78a203e45840
moviebazaa <69888886+moviebazaa@users.noreply.github.com>
2023-06-04 20:57:39 +0530

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+def main():
+    st.title("My Streamlit Web App")
+    st.write("Welcome to my web app!")
+
+    # Add more Streamlit components and code here as needed
+
+if __name__ == "__main__":
+    main()
